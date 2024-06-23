206882ba4eb9dc4fe135ab7da85691f754f6c694
Shubham Pawar <shubhampwr9945@gmail.com>
2024-06-23 14:10:31 +0530

    rand

--- src/Helper/PosterCard/index.jsx
+++ src/Helper/PosterCard/index.jsx
@@ -1,14 +1,18 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const PosterCard = ({posterImg, posterName, posterPath, className}) => {
     let posterClass = classNames(className, "poster-card")
+    let [loaded, setLoaded] = useState(false)
+    const loaderHandler = () => {
+      setLoaded(true)
+    }
   return (
     
     <div className={posterClass}>
         <Link to={posterPath}>
-            <img src={posterImg} alt={posterName} />
+            <img src={loaded? posterImg: "https://via.placeholder.com/300"} alt={posterName} onLoad={loaderHandler} />
         </Link>
     </div>
   )
