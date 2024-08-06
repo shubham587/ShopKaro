6c776ab52ff7f5111bee9f48b97f4984ce1aff8e
shubham <shubhampwr9945@gmail.com>
2024-08-06 15:21:01 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@
 import "./App.sass";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import IdComp from "./Component/IdComp";
-// import DefaultLayout from "./Pages/Layout/DefaultLayout.jsx";
-const DefaultLayout = lazy(() => import("./Pages/Layout/DefaultLayout.jsx"))
-// import HeroSection from "./Pages/HeroSection/index.jsx";
-const HeroSection = lazy(() => import("./Pages/HeroSection/index.jsx"))
-// import WomenHero from "./Pages/WomenSection/index.jsx";
-const WomenHero = lazy(() => import("./Pages/WomenSection/index.jsx"))
-// import MenHero from "./Pages/MenSection/index.jsx";
-const MenHero = lazy(() => import("./Pages/MenSection/index.jsx"))
-// import KidsHero from "./Pages/KidsSection/index.jsx";
+import DefaultLayout from "./Pages/Layout/DefaultLayout.jsx";
+// const DefaultLayout = lazy(() => import("./Pages/Layout/DefaultLayout.jsx"))
+import HeroSection from "./Pages/HeroSection/index.jsx";
+// const HeroSection = lazy(() => import("./Pages/HeroSection/index.jsx"))
+import WomenHero from "./Pages/WomenSection/index.jsx";
+// const WomenHero = lazy(() => import("./Pages/WomenSection/index.jsx"))
+import MenHero from "./Pages/MenSection/index.jsx";
+// const MenHero = lazy(() => import("./Pages/MenSection/index.jsx"))
+import KidsHero from "./Pages/KidsSection/index.jsx";
 import Card from "./Helper/Card/index.jsx";
-const KidsHero = lazy(() => import("./Pages/KidsSection/index.jsx"))
+// const KidsHero = lazy(() => import("./Pages/KidsSection/index.jsx"))
 import WomenClothingPage, {
   loader as womenLoader,
 } from "./Pages/WomenClothing/index.jsx";
